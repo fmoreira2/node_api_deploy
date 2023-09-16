88439c8a477a21efe9af639b254f6a93db25c01c
Fernando Moreira <fernando.m.silva@outlook.com>
2023-09-16 10:44:41 -0300

    api_node_zero_ao_deploy

---
+++ src/server.ts
@@ -0,0 +1,43 @@
+import { PrismaClient } from "@prisma/client";
+import fastify from "fastify";
+import {z} from "zod"; 
+
+//criando uma instância do fastify
+const app = fastify();
+
+//iniciando o banco de dados com prisma
+const prisma = new PrismaClient();
+
+
+//criando minhas rotas
+//retorna todos os usuários
+app.get("/users", async (request, reply) => {
+    const users = await prisma.user.findMany();
+    return users;
+});
+//cria um novo usuário
+app.post("/users", async (request, reply) => {
+    //validação dos dados com zod
+    const createUserSchema = z.object({
+        name: z.string(),
+        email: z.string().email()
+    });
+
+    const { name, email } = createUserSchema.parse(request.body);
+    const user = await prisma.user.create({
+        data: {
+            name,
+            email
+        }
+    });
+    //return user;
+    return reply.status(201).send(user);
+});
+
+//iniciando o servidor
+app.listen({
+    host: '0.0.0.0',
+    port: process.env.PORT? Number(process.env.port) : 3000
+}).then(()=>{
+    console.log("Servidor iniciado com sucesso!");
+});
