56705a24374d433b4543dad21e71f9acb0346637
Fernando Moreira <fernando.m.silva@outlook.com>
2023-09-17 15:22:26 -0300

    api_node_deploy

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 import fastify from "fastify";
-import {z} from "zod"; 
+import { type } from "os";
+import { z } from "zod";
 
 //criando uma instância do fastify
 const app = fastify();
@@ -8,36 +9,42 @@ const app = fastify();
 //iniciando o banco de dados com prisma
 const prisma = new PrismaClient();
 
-
 //criando minhas rotas
 //retorna todos os usuários
 app.get("/users", async (request, reply) => {
-    const users = await prisma.user.findMany();
-    return users;
+  const users = await prisma.user.findMany();
+  return users;
 });
+//retorna um usuário específico por id
+app.get("/users/:id", async (request, reply) => {
+  return "teste";
+});
+
 //cria um novo usuário
 app.post("/users", async (request, reply) => {
-    //validação dos dados com zod
-    const createUserSchema = z.object({
-        name: z.string(),
-        email: z.string().email()
-    });
+  //validação dos dados com zod
+  const createUserSchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+  });
 
-    const { name, email } = createUserSchema.parse(request.body);
-    const user = await prisma.user.create({
-        data: {
-            name,
-            email
-        }
-    });
-    //return user;
-    return reply.status(201).send(user);
+  const { name, email } = createUserSchema.parse(request.body);
+  const user = await prisma.user.create({
+    data: {
+      name,
+      email,
+    },
+  });
+  //return user;
+  return reply.status(201).send(user);
 });
 
 //iniciando o servidor
-app.listen({
-    host: '0.0.0.0',
-    port: process.env.PORT? Number(process.env.port) : 3000
-}).then(()=>{
+app
+  .listen({
+    host: "0.0.0.0",
+    port: process.env.PORT ? Number(process.env.port) : 3000,
+  })
+  .then(() => {
     console.log("Servidor iniciado com sucesso!");
-});
+  });
